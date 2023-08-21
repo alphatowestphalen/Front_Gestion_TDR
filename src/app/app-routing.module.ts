b0d60c47703e83d78b6f55e24310820679f6ddb7
alphatowestphalen <alphato2021@gmail.com>
2023-08-21 05:28:46 +0300

    add path initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostComponent } from './controler/post/post.component';
+import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
-  {path: 'post', component: PostComponent}
+  {path: 'post', component: PostComponent},
+  {path: '/', component: HomeComponent},
 ];
 
 @NgModule({
