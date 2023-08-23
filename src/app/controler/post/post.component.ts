49a2a8fdd9dfe43943de09e45058518a3b31e877
alphatowestphalen <alphato2021@gmail.com>
2023-08-23 23:39:26 +0300

    update and menage Idpost in post

--- src/app/controler/post/post.component.ts
+++ src/app/controler/post/post.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Post } from 'src/app/model/Post';
 import { PostService } from 'src/app/service/post.service';
 
@@ -10,28 +12,61 @@ import { PostService } from 'src/app/service/post.service';
 })
 export class PostComponent  implements OnInit{
   listPost: Post[] = [];
+  newPost: Post = { id: 0, description:""};
+  idPost: number = 0;
+  filteredTableData = [...this.listPost];
 
-  idPost: string ="";
-
-  constructor(private postService: PostService, private route: ActivatedRoute){}
+  constructor(private postService: PostService){}
 
   ngOnInit(): void {
-    this.getPostsAll()
+
+    this.getPostsAll();
   }
   
-  getPostsAll() {
+  public getPostsAll(){
       return this.postService.getPost().subscribe(data =>{
         this.listPost = data
       }
       
   )}
-  deletePostControler(idPost: number): void{
-    console.log('====================================');
-    console.log(idPost);
-    console.log('====================================');
-    this.postService.DeletePostService(idPost).subscribe(() =>{
+  
+  public savePost( ):void {
+    this.postService.savePost(this.newPost).subscribe(()=>{
       this.getPostsAll();
+    }
+    )
+  }
+  filterTable(event: any): void {
+    const value = event.target.value.toLowerCase();
+    this.filteredTableData = this.listPost.filter(row =>
+      (value) !== -1
+    );
+  }
+    public finByIdControler(id:number){
+      return this.postService.findById(id).subscribe(data =>{
+     this.newPost = {id:0, description:""};
+        this.newPost = data;
+      });
+    }
+
+  public updatePost(){
+   this.postService.updatePost(this.newPost).subscribe(data =>{
+     this.getPostsAll();
+
+   });
+  }
+  
+  public deletePostControler(idPost: number): void{
+      this.postService.DeletePostService(idPost).subscribe(() =>{
+        this.getPostsAll();
     })
+  }
+
+  public getFindEndId(){
+    this.newPost = {id: 0, description:""}
+      return this.postService.getLatestPostService().subscribe(data=>{
+        this.newPost.id = data.id + 1; 
+      })
   }
   
 }
--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environment/environement';
 import { Post } from '../model/Post';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +12,37 @@ import { ActivatedRoute } from '@angular/router';
 export class PostService {
   private _url = environment.url;
   private idPost: string ="";
-
+  private post:Post[] = [];
   constructor(private _http: HttpClient) { }
   
-  
+  // get all poste
   getPost(){
     return this._http.get<Post[]>( this._url + "/posts")
   }
+
+// Add post 
+  public savePost(post:Post): Observable<Post>{
+    return this._http.post<Post>( this._url + "/post", post);
+  }
+
+  // update poste
+  public updatePost(post:Post): Observable<Post>{ 
+    return this._http.put<Post>(this._url + "/post/"+post.id, post);
+  }
+
+  // find by Id
+  public findById(id:number): Observable<Post>{
+    return this._http.get<Post>(this._url +"/post/" + id);
+}
+
+// delete poste
   DeletePostService(id: number){
     return this._http.delete <Post[]>(this._url+ "/post/"+id);
   }
+
+// findLatestPost
+  public getLatestPostService(): Observable<Post>{
+    return this._http.get <Post>(this._url+ "/post/EndId");
+  }
+  
 }
